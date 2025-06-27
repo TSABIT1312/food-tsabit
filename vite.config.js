febe5ce35fbe9c091d1e7a6704e02bbdb6074cd8
tsabit <sabitalfikri@gmail.com>
2025-06-27 21:38:11 +0700

    bismillah dadi

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // <-- penting untuk Vercel agar tidak salah path
+  base: '/', // <-- penting untuk Vercel agar tidak salah path
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
